837e851a8d1d5ba93482ad5e9f4ef0b7de98a534
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-07-24 14:23:35 -0600

    Black format

--- Docs/sphinx/conf.py
+++ Docs/sphinx/conf.py
@@ -1,33 +1,38 @@
 import sys
 
-extensions = [ 'sphinx.ext.mathjax', 'breathe']
-templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'SPADES'
-copyright = u'2023, the Alliance for Sustainable Energy, LLC., through National Renewable Energy Laboratory. All rights reserved'
-author = u'E. Young, M. T. Henry de Frahan, H. Sitaraman, R. Larsen'
-version = u'0.1'
-release = u'0.1'
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-pygments_style = 'sphinx'
+extensions = ["sphinx.ext.mathjax", "breathe", "exhale"]
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+project = "SPADES"
+copyright = "2023, the Alliance for Sustainable Energy, LLC., through National Renewable Energy Laboratory. All rights reserved"
+author = "M. T. Henry de Frahan, E. Young, H. Sitaraman, R. Larsen, D. Vaidhynathan"
+version = "0.1"
+release = "0.1"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+pygments_style = "sphinx"
 todo_include_todos = False
 numfig = True
-numfig_format = {'figure': '%s', 'table': '%s', 'code-block': '%s'}
-html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
-htmlhelp_basename = 'spadesdoc'
-latex_elements = { }
+numfig_format = {"figure": "%s", "table": "%s", "code-block": "%s"}
+html_theme = "sphinx_rtd_theme"
+# html_static_path = ['_static']
+htmlhelp_basename = "spadesdoc"
+latex_elements = {}
 latex_documents = [
-    (master_doc, 'spades.tex', u'SPADES Documentation',
-     author, 'manual'),
+    (master_doc, "spades.tex", "SPADES Documentation", author, "manual"),
 ]
 texinfo_documents = [
-    (master_doc, 'spades', u'SPADES Documentation',
-     author, 'SPADES', 'Scalable PArallel Discrete Event Simulation.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "spades",
+        "SPADES Documentation",
+        author,
+        "SPADES",
+        "Scalable PArallel Discrete Event Simulation.",
+        "Miscellaneous",
+    ),
 ]
 
-nitpick_ignore_regex = [(r'cpp:identifier', r'amrex.*')]
+nitpick_ignore_regex = [(r"cpp:identifier", r"amrex.*")]
 
-breathe_default_project = 'spades'
+breathe_default_project = "spades"
